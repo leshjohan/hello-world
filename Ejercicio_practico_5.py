c238d4c6bf3c6eefdf788b66499b0c468caccca6
Lesh Mendoza <lesh.johan@gmail.com>
2022-04-25 17:40:36 +0200

    Create Ejercicio_practico_5.py

---
+++ Ejercicio_practico_5.py
@@ -0,0 +1,22 @@
+x,y=0,1
+while y<=1597:
+    print(x, y, end=" ")
+    x=x+y
+    y=x+y
+
+"""imprime x(0) y(1)
+Luego, hace la suma:
+x=0+1
+y=1+1
+
+La siguiente iteración sería:
+imprime x(1) y(2)
+Luego, hace la suma:
+x=1+2
+y=3+2
+
+La siguiente iteración sería:
+imprime x(3) y(5)
+Luego, suma:
+x=3+5
+y=8+5"""
